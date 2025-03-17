64fd303e664e53111874f03b9026540efd3b48ef
Alex Petrov <alex.petrov.vt@gmail.com>
2025-03-17 09:13:54 -0400

    feat: add since query param
    According to golang-index spec [1], the server must support 'since' and
    'limit' query params to limit the number of listed repo tags. This
    commit adds support for the 'since' param.
    
    [1] https://index.golang.org

---
+++ server.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+)
+
+type server struct {
+	port int
+	idx  *index
+}
+
+func newServer(port int, index *index) *server {
+	return &server{port: port, idx: index}
+}
+
+type module struct {
+	Path      string `json:"Path"`
+	Version   string `json:"Version"`
+	Timestamp string `json:"Timestamp"`
+}
+
+func (s *server) handleIndex(w http.ResponseWriter, r *http.Request) {
+	s.idx.mu.RLock()
+	defer s.idx.mu.RUnlock()
+
+	var since time.Time
+	var err error
+	if sinceParam := r.URL.Query().Get("since"); sinceParam != "" {
+		since, err = time.Parse(time.RFC3339, sinceParam)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("error converting 'since' param %s: %v", sinceParam, err), http.StatusBadRequest)
+			return
+		}
+	}
+
+	var lines []string
+	for repoName, tags := range s.idx.repoTags {
+		for _, tag := range tags {
+			if tag.commitDate.Before(since) {
+				continue
+			}
+
+			out, err := json.Marshal(&module{
+				Path:      fmt.Sprintf("github.netflix.net/%s", repoName),
+				Version:   tag.tag,
+				Timestamp: tag.commitDate.Format(time.RFC3339),
+			})
+			if err != nil {
+				http.Error(w, fmt.Sprintf("error marshalling response for tag %s: %v", tag.tag, err), http.StatusInternalServerError)
+				return
+			}
+
+			lines = append(lines, string(out))
+		}
+	}
+	if _, err := fmt.Fprint(w, strings.Join(lines, "\n")); err != nil {
+		http.Error(w, fmt.Sprintf("error writing response: %v", err), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (s *server) listenAndServe() error {
+	http.HandleFunc("/", s.handleIndex)
+	fmt.Printf("Server listening on :%d\n", s.port)
+	return http.ListenAndServe(fmt.Sprintf(":%d", s.port), nil)
+}
---
+++ server_test.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestIndexHandler(t *testing.T) {
+	fakedRepos := map[string][]*repoTag{
+		"repo1": []*repoTag{
+			{tag: "tag1", commitDate: time.Date(2025, 1, 2, 3, 4, 5, 6, time.UTC)},
+			{tag: "tag2", commitDate: time.Date(2025, 2, 3, 4, 5, 6, 7, time.UTC)},
+			{tag: "tag3", commitDate: time.Date(2025, 3, 4, 5, 6, 7, 8, time.UTC)},
+		},
+	}
+
+	for _, tc := range []struct {
+		name           string
+		sinceParam     string
+		tags           map[string][]*repoTag
+		wantStatusCode int
+		wantResponse   string
+	}{
+		{
+			name:           "empty response",
+			wantStatusCode: http.StatusOK,
+		},
+		{
+			name:           "response with tags",
+			tags:           fakedRepos,
+			wantStatusCode: http.StatusOK,
+			wantResponse: "" +
+				`{"Path":"github.netflix.net/repo1","Version":"tag1","Timestamp":"2025-01-02T03:04:05Z"}` + "\n" +
+				`{"Path":"github.netflix.net/repo1","Version":"tag2","Timestamp":"2025-02-03T04:05:06Z"}` + "\n" +
+				`{"Path":"github.netflix.net/repo1","Version":"tag3","Timestamp":"2025-03-04T05:06:07Z"}`,
+		},
+		{
+			name:           "with 'since' query param",
+			sinceParam:     "2025-02-01T00:00:00Z",
+			tags:           fakedRepos,
+			wantStatusCode: http.StatusOK,
+			wantResponse: "" +
+				`{"Path":"github.netflix.net/repo1","Version":"tag2","Timestamp":"2025-02-03T04:05:06Z"}` + "\n" +
+				`{"Path":"github.netflix.net/repo1","Version":"tag3","Timestamp":"2025-03-04T05:06:07Z"}`,
+		},
+		{
+			name:           "with invalid 'since' query param",
+			sinceParam:     "invalid",
+			tags:           fakedRepos,
+			wantStatusCode: http.StatusBadRequest,
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			s := newServer(0, &index{repoTags: tc.tags})
+
+			request := httptest.NewRequest(http.MethodGet, "/", nil)
+			query := request.URL.Query()
+			if tc.sinceParam != "" {
+				query.Add("since", tc.sinceParam)
+			}
+			request.URL.RawQuery = query.Encode()
+
+			recorder := httptest.NewRecorder()
+
+			s.handleIndex(recorder, request)
+
+			if tc.wantStatusCode != recorder.Code {
+				t.Errorf("wanted status code %d, got %d", tc.wantStatusCode, recorder.Code)
+			}
+			if tc.wantStatusCode == http.StatusOK {
+				body, err := io.ReadAll(recorder.Body)
+				if err != nil {
+					t.Errorf("unexpected error while reading recorder body: %v", err)
+				}
+				if tc.wantResponse != string(body) {
+					t.Errorf("unexpected reponse: -want, +got: %s", cmp.Diff(tc.wantResponse, string(body)))
+				}
+			}
+		})
+	}
+}
