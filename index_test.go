f81e20b2d76b7e9b68508376fa867091b228e68d
Alex Petrov <alex.petrov.vt@gmail.com>
2025-04-22 08:24:30 -0400

    refactor: many minor fixes and quality of life improvements

--- index_test.go
+++ index_test.go
@@ -19,7 +19,7 @@ type mockGithubClient struct {
 	stubbedResults []interface{}
 }
 
-func (m *mockGithubClient) Query(ctx context.Context, q interface{}, variables map[string]interface{}) error {
+func (m *mockGithubClient) Query(ctx context.Context, query interface{}, variables map[string]interface{}) error {
 	if len(m.stubbedResults) == 0 {
 		return nil
 	}
@@ -31,204 +31,162 @@ func (m *mockGithubClient) Query(ctx context.Context, q interface{}, variables m
 	// behavior. resultsIdx keeps track of which step in the multistep query
 	// we're in.
 
-	stubQueryResponse := reflect.ValueOf(q)
+	stubQueryResponse := reflect.ValueOf(query)
 	stubQueryResponse = stubQueryResponse.Elem()
 	stubQueryResponse.Set(reflect.ValueOf(m.stubbedResults[m.resultsIdx]))
 	m.resultsIdx++
 	return nil
 }
 
-func TestQueryingRepos(t *testing.T) {
-	type responses []struct {
+func TestRepos_EmptyResponse(t *testing.T) {
+	index := newIndex(&mockGithubClient{})
+	resultsChan := make(chan string, 0)
+
+	index.repos(t.Context(), resultsChan)
+
+	if len(resultsChan) != 0 {
+		t.Errorf("expected channel to be empty but it has %d results", len(resultsChan))
+	}
+}
+
+func TestRepos_MultiplePages(t *testing.T) {
+	responses := []struct {
 		reposURLs   []string
 		endCursor   githubv4.String
 		hasNextPage bool
-	}
-
-	for _, tc := range []struct {
-		name        string
-		responses   responses
-		wantResults []string
 	}{
 		{
-			name: "empty response",
+			reposURLs: []string{
+				"https://github.netflix.net/corp/ftl-proxy",
+				"https://github.netflix.net/corp/cloudgaming-ocgactl",
+				"https://github.netflix.net/corp/cloudgaming-moby-fork",
+			},
+			hasNextPage: true,
+			endCursor:   "somecursor",
 		},
 		{
-			name: "single page",
-			responses: responses{
-				{
-					reposURLs: []string{
-						"https://github.netflix.net/corp/ftl-proxy",
-						"https://github.netflix.net/corp/cloudgaming-ocgactl",
-						"https://github.netflix.net/corp/cloudgaming-moby-fork",
-					},
-				},
-			},
-			wantResults: []string{
-				"corp/ftl-proxy",
-				"corp/cloudgaming-ocgactl",
-				"corp/cloudgaming-moby-fork",
+			reposURLs: []string{
+				"https://github.netflix.net/corp/cloudgaming-tdd-grafana",
+				"https://github.netflix.net/corp/cloudgaming-game-input-go",
+				"https://github.netflix.net/corp/cpie-proxyd",
 			},
 		},
-		{
-			name: "multiple pages",
-			responses: responses{
-				{
-					reposURLs: []string{
-						"https://github.netflix.net/corp/ftl-proxy",
-						"https://github.netflix.net/corp/cloudgaming-ocgactl",
-						"https://github.netflix.net/corp/cloudgaming-moby-fork",
-					},
-					hasNextPage: true,
-					endCursor:   "somecursor",
-				},
-				{
-					reposURLs: []string{
-						"https://github.netflix.net/corp/cloudgaming-tdd-grafana",
-						"https://github.netflix.net/corp/cloudgaming-game-input-go",
-						"https://github.netflix.net/corp/cpie-proxyd",
-					},
-				},
-			},
-			wantResults: []string{
-				"corp/ftl-proxy",
-				"corp/cloudgaming-ocgactl",
-				"corp/cloudgaming-moby-fork",
-				"corp/cloudgaming-tdd-grafana",
-				"corp/cloudgaming-game-input-go",
-				"corp/cpie-proxyd",
-			},
-		},
-	} {
-		t.Run(tc.name, func(t *testing.T) {
-			var stubbedResponses []interface{}
-			for _, response := range tc.responses {
-				response, err := buildRepoQueryResponse(response.reposURLs, response.endCursor, response.hasNextPage)
-				if err != nil {
-					t.Fatalf("unexpected error building query response: %v", err)
-				}
-				stubbedResponses = append(stubbedResponses, response)
-			}
+	}
 
-			index := newIndex(context.Background(), &mockGithubClient{stubbedResults: stubbedResponses})
-			resultsChan := make(chan string, len(tc.wantResults))
+	var stubbedResponses []interface{}
+	for _, response := range responses {
+		response := buildRepoQueryResponse(t, response.reposURLs, response.endCursor, response.hasNextPage)
+		stubbedResponses = append(stubbedResponses, response)
+	}
 
-			index.repos(context.Background(), resultsChan)
+	wantResults := []string{
+		"corp/ftl-proxy",
+		"corp/cloudgaming-ocgactl",
+		"corp/cloudgaming-moby-fork",
+		"corp/cloudgaming-tdd-grafana",
+		"corp/cloudgaming-game-input-go",
+		"corp/cpie-proxyd",
+	}
 
-			var results []string
-			for r := range resultsChan {
-				results = append(results, r)
-			}
+	index := newIndex(&mockGithubClient{stubbedResults: stubbedResponses})
+	resultsChan := make(chan string, len(wantResults))
 
-			if !cmp.Equal(tc.wantResults, results) {
-				t.Errorf("want results: %v, got: %v", tc.wantResults, results)
-			}
-		})
+	index.repos(t.Context(), resultsChan)
+
+	var results []string
+	for r := range resultsChan {
+		results = append(results, r)
+	}
+
+	if diff := cmp.Diff(wantResults, results); diff != "" {
+		t.Errorf("unexpected results from repos: -want +got: %s", diff)
 	}
 }
 
-func TestQueryingTags(t *testing.T) {
-	type responses []struct {
+func TestTagsForRepos_EmptyResponse(t *testing.T) {
+	repos := make(chan string, 1)
+	repos <- "corp/repo1"
+	close(repos)
+
+	index := newIndex(&mockGithubClient{})
+	err := index.tagsForRepos(t.Context(), repos)
+	if err != nil {
+		t.Fatalf("unexpected error generating tags for repos: %v", err)
+	}
+
+	gotTags := index.repoTags["corp/repo1"]
+
+	if len(gotTags) != 0 {
+		t.Errorf("expected no tags, but got %d results", len(gotTags))
+	}
+}
+
+func TestTagsForRepos_MultiplePages(t *testing.T) {
+	date := time.Date(2025, 1, 2, 3, 4, 5, 6, time.UTC)
+	responses := []struct {
 		tags        []repoTag
 		endCursor   githubv4.String
 		hasNextPage bool
-	}
-
-	date := time.Date(2025, 1, 2, 3, 4, 5, 6, time.UTC)
-
-	for _, tc := range []struct {
-		name      string
-		responses responses
-		wantTags  map[string][]*repoTag
 	}{
 		{
-			name: "empty response",
-			wantTags: map[string][]*repoTag{
-				"corp/repo1": nil,
+			tags: []repoTag{
+				{tag: "_gheMigrationPR-435", commitDate: date},
+				{tag: "_gheMigrationPR-436", commitDate: date},
+				{tag: "_gheMigrationPR-437", commitDate: date},
 			},
+			endCursor:   "somecursor",
+			hasNextPage: true,
 		},
 		{
-			name: "single page",
-			responses: responses{
-				{
-					tags: []repoTag{
-						{tag: "_gheMigrationPR-435", commitDate: date},
-						{tag: "_gheMigrationPR-436", commitDate: date},
-						{tag: "_gheMigrationPR-437", commitDate: date},
-					},
-				},
-			},
-			wantTags: map[string][]*repoTag{
-				"corp/repo1": []*repoTag{
-					{tag: "_gheMigrationPR-435", commitDate: date},
-					{tag: "_gheMigrationPR-436", commitDate: date},
-					{tag: "_gheMigrationPR-437", commitDate: date},
-				},
+			tags: []repoTag{
+				{tag: "_gheMigrationPR-438", commitDate: date},
+				{tag: "_gheMigrationPR-439", commitDate: date},
+				{tag: "_gheMigrationPR-430", commitDate: date},
 			},
 		},
-		{
-			name: "multiple pages",
-			responses: responses{
-				{
-					tags: []repoTag{
-						{tag: "_gheMigrationPR-435", commitDate: date},
-						{tag: "_gheMigrationPR-436", commitDate: date},
-						{tag: "_gheMigrationPR-437", commitDate: date},
-					},
-					endCursor:   "somecursor",
-					hasNextPage: true,
-				},
-				{
-					tags: []repoTag{
-						{tag: "_gheMigrationPR-438", commitDate: date},
-						{tag: "_gheMigrationPR-439", commitDate: date},
-						{tag: "_gheMigrationPR-430", commitDate: date},
-					},
-				},
-			},
-			wantTags: map[string][]*repoTag{
-				"corp/repo1": []*repoTag{
-					{tag: "_gheMigrationPR-435", commitDate: date},
-					{tag: "_gheMigrationPR-436", commitDate: date},
-					{tag: "_gheMigrationPR-437", commitDate: date},
-					{tag: "_gheMigrationPR-438", commitDate: date},
-					{tag: "_gheMigrationPR-439", commitDate: date},
-					{tag: "_gheMigrationPR-430", commitDate: date},
-				},
-			},
+	}
+
+	repos := make(chan string, 1)
+	repos <- "corp/repo1"
+	close(repos)
+
+	var stubbedResponses []interface{}
+	for _, response := range responses {
+		stubbedResponses = append(stubbedResponses, buildTagsQueryResponse(response.tags, response.endCursor, response.hasNextPage))
+	}
+
+	wantTags := map[string][]*repoTag{
+		"corp/repo1": []*repoTag{
+			{tag: "_gheMigrationPR-435", commitDate: date},
+			{tag: "_gheMigrationPR-436", commitDate: date},
+			{tag: "_gheMigrationPR-437", commitDate: date},
+			{tag: "_gheMigrationPR-438", commitDate: date},
+			{tag: "_gheMigrationPR-439", commitDate: date},
+			{tag: "_gheMigrationPR-430", commitDate: date},
 		},
-	} {
-		t.Run(tc.name, func(t *testing.T) {
-			repos := make(chan string, 1)
-			repos <- "corp/repo1"
-			close(repos)
+	}
 
-			var stubbedResponses []interface{}
-			for _, response := range tc.responses {
-				stubbedResponses = append(stubbedResponses, buildTagsQueryResponse(response.tags, response.endCursor, response.hasNextPage))
-			}
+	index := newIndex(&mockGithubClient{stubbedResults: stubbedResponses})
+	err := index.tagsForRepos(t.Context(), repos)
+	if err != nil {
+		t.Fatalf("unexpected error generating tags for repos: %v", err)
+	}
 
-			index := newIndex(context.Background(), &mockGithubClient{stubbedResults: stubbedResponses})
-			err := index.tagsForRepos(context.Background(), repos)
-			if err != nil {
-				t.Fatalf("unexpected error generating tags for repos: %v", err)
-			}
-
-			if !cmp.Equal(tc.wantTags, index.repoTags, cmp.AllowUnexported(repoTag{})) {
-				t.Errorf("wanted tags: %v, got: %v", tc.wantTags, index.repoTags)
-			}
-		})
+	if !cmp.Equal(wantTags, index.repoTags, cmp.AllowUnexported(repoTag{})) {
+		t.Errorf("wanted tags: %v, got: %v", wantTags, index.repoTags)
 	}
 }
 
-func buildRepoQueryResponse(reposURLs []string, endCursor githubv4.String, hasNextPage bool) (interface{}, error) {
+func buildRepoQueryResponse(t *testing.T, reposURLs []string, endCursor githubv4.String, hasNextPage bool) repoQueryResult {
+	t.Helper()
+
 	var edges []repoQueryEdge
 
 	for _, repoURL := range reposURLs {
 		var edge repoQueryEdge
 		url, err := url.Parse(repoURL)
 		if err != nil {
-			return nil, err
+			t.Fatalf("error parsing repo url: %v", err)
 		}
 
 		edge.Node.Repo.URL = *githubv4.NewURI(githubv4.URI{URL: url})
@@ -239,10 +197,10 @@ func buildRepoQueryResponse(reposURLs []string, endCursor githubv4.String, hasNe
 	q.Search.Edges = edges
 	q.Search.PageInfo.EndCursor = endCursor
 	q.Search.PageInfo.HasNextPage = hasNextPage
-	return q, nil
+	return q
 }
 
-func buildTagsQueryResponse(tags []repoTag, endCursor githubv4.String, hasNextPage bool) interface{} {
+func buildTagsQueryResponse(tags []repoTag, endCursor githubv4.String, hasNextPage bool) tagQueryResponse {
 	var edges []tagQueryEdge
 
 	for _, tag := range tags {
